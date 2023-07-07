45c54dbe71cb7e5494a7bd4224d122ec191ba86b
Migalhaz <105467709+Migalhaz@users.noreply.github.com>
2023-07-07 20:33:52 -0300

    Drawer

---
+++ Assets/Scripts/Miguel/Drawer.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.GameSystem.Interact
+{
+    public class Drawer : MonoBehaviour, IInteract
+    {
+        [SerializeField, Range(0, 10)] float m_animTime;
+        [SerializeField] float m_zPositionBuff;
+        Vector3 m_openedPosition;
+        Vector3 m_closedPosition;
+
+        bool m_opened = false;
+        private void Start()
+        {
+            m_opened = false;
+            m_closedPosition = transform.position;
+            m_openedPosition.Set(m_closedPosition.x, m_closedPosition.y, m_closedPosition.z + m_zPositionBuff);
+        }
+
+        void Update()
+        {
+            Vector3 newPosition = m_opened ? m_openedPosition : m_closedPosition;
+            transform.position = Vector3.Lerp(transform.position, newPosition, m_animTime * Time.deltaTime);
+        }
+
+        public virtual void Interact()
+        {
+            ChangeState();
+        }
+
+        void ChangeState()
+        {
+            if (m_opened)
+            {
+                Close();
+            }
+            else
+            {
+                Open();
+            }
+        }
+
+        void Open()
+        {
+            m_opened = true;
+        }
+
+        void Close()
+        {
+            m_opened = false;
+        }
+    }
+}
