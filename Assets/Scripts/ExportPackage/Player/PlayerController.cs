27d3ce9f16caffcb586f34fb1b9514a0a8f5259c
Migalhaz <105467709+Migalhaz@users.noreply.github.com>
2023-07-07 19:01:45 -0300

    Player Controller Inputs e Interact

---
+++ Assets/Scripts/ExportPackage/Player/PlayerController.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.GameSystem.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [Header("Player Settings")]
+        [SerializeField] float moveSpeed = 10f;
+
+        [Header("Input Settings")]
+        [SerializeField] PlayerInputs playerInputManager;
+        [SerializeField, Min(1)] float mouseSmoothness = 100;
+        [SerializeField] Vector2 mouseSense = new(2, 2);
+        [SerializeField] Vector2 maxYView = new(-30, 30);
+        float rY;
+        float rX;
+
+        [Header("Transforms")]
+        [SerializeField] Transform playerTransform;
+        [SerializeField] Transform camPivot;
+        [SerializeField] Transform mainCam;
+        //[SerializeField] Transform itemRendererCam;
+        Vector3 dir;
+        Vector3 inputs;
+        Rigidbody rig;
+
+        [Header("Editor Only")]
+        bool followMouse = true;
+
+
+        private void Awake()
+        {
+            playerInputManager ??= gameObject.TryGetComponent(out PlayerInputs _newPlayerInput) ? _newPlayerInput : gameObject.AddComponent<PlayerInputs>();
+            rig = GetComponent<Rigidbody>();
+            
+        }
+
+        private void Start()
+        {
+            SetFollowMouse(true);
+        }
+
+        private void Update()
+        {
+            InputListener();
+            Move();
+        }
+
+        private void FixedUpdate()
+        {
+            FixedMove();
+        }
+
+        void FixedMove()
+        {
+            rig.AddForce(dir * moveSpeed, ForceMode.Force);
+        }
+
+        void Move()
+        {
+            if (playerInputManager.PauseCamPressed()) SetFollowMouse(!followMouse);
+            if (!followMouse) return;
+            playerTransform.Rotate(0, rX, 0, Space.World);
+
+            
+
+            mainCam.rotation = Quaternion.Lerp(mainCam.rotation, Quaternion.Euler(rY * mouseSense.y, playerTransform.eulerAngles.y, 0f), mouseSmoothness * Time.deltaTime);
+            //itemRendererCam.rotation = mainCam.rotation;
+            camPivot.position = Vector3.Lerp(camPivot.position, playerTransform.position, moveSpeed * Time.deltaTime);
+
+            
+        }
+
+        public void SetFollowMouse(bool _newValue)
+        {
+            
+            followMouse = _newValue;
+
+            Cursor.lockState = followMouse ? CursorLockMode.Locked : CursorLockMode.Confined;
+        }
+
+        void InputListener()
+        {
+            //inputs.Set(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+            inputs.Set(playerInputManager.MoveInput().x, 0f, playerInputManager.MoveInput().y);
+            dir = playerTransform.TransformVector(inputs.normalized);
+            rX = Mathf.Lerp(rX, playerInputManager.MousePositionInput().x * mouseSense.x, mouseSmoothness * Time.deltaTime);
+            rY = Mathf.Clamp(rY - playerInputManager.MousePositionInput().y * mouseSense.y * mouseSmoothness * Time.deltaTime, maxYView.x, maxYView.y);
+            
+        //    rX = Mathf.Lerp(rX, Input.GetAxisRaw("Mouse X") * mouseSense.x, mouseSmoothness * Time.deltaTime);
+        //    rY = Mathf.Clamp(rY - Input.GetAxisRaw("Mouse Y") * mouseSense.y * mouseSmoothness * Time.deltaTime, maxYView.x, maxYView.y);
+        }
+    }
+}
---
+++ Assets/Scripts/ExportPackage/Player/PlayerRaycast.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.GameSystem.Player
+{
+    public class PlayerRaycast : MonoBehaviour
+    {
+        [Header("Input Manager")]
+        [SerializeField] PlayerInputs playerInputManager;
+
+        [Header("Raycast Settings")]
+        [SerializeField] Transform camPos;
+        [SerializeField] float raycastDistance = 10f;
+        RaycastHit hit;
+        [SerializeField] LayerMask hitLayerMask;
+        
+#if UNITY_EDITOR
+        [Header("Gizmos Settings")]
+        [SerializeField] Color colorInHit = Color.green;
+        [SerializeField] Color colorOutHit = Color.red;
+#endif
+        private void Awake()
+        {
+            playerInputManager ??= gameObject.TryGetComponent(out PlayerInputs _newPlayerInput) ? _newPlayerInput : gameObject.AddComponent<PlayerInputs>();
+        }
+
+        void Update()
+        {
+            if (!InteractLogic()) return;
+            PlayerInteract();
+        }
+
+        bool InteractLogic()
+        {
+            //if (!Input.GetKeyDown(interactKeycode)) return false;
+            //if (hit.transform == null) return false;
+            //return true
+            return (playerInputManager.InteractKeycodePressed() && hit.transform != null); 
+        }
+
+        void PlayerInteract()
+        {
+            if (hit.transform.TryGetComponent(out Interact.IInteract _component))
+            {
+                _component.Interact();
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            Physics.Raycast(camPos.position, camPos.transform.forward, out hit, raycastDistance, hitLayerMask);
+#if UNITY_EDITOR
+            Debug.DrawRay(camPos.position, camPos.transform.forward * raycastDistance, hit.transform == null ? colorOutHit : colorInHit);
+#endif
+        }
+    }
+}
+
+namespace Game.GameSystem.Interact
+{
+    public interface IInteract
+    {
+        void Interact();
+    }
+}
